958ade50790bab9339a5a3610d11897f8e1642f0
ejaaskel <esa.jaaskela@suomi24.fi>
2024-06-30 08:42:57 +0300

    Override STM HAL USB FS handler with TinyUSB
    To integrate TinyUSB stack into the project, call the TinyUSB interrupt
    handler instead of the STM HAL interrupt handler and return early to
    avoid calling the wrong handler.

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -22,6 +22,7 @@
 #include "stm32f4xx_it.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "tusb.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -204,7 +205,8 @@ void SysTick_Handler(void)
 void OTG_FS_IRQHandler(void)
 {
   /* USER CODE BEGIN OTG_FS_IRQn 0 */
-
+  tud_int_handler(0);
+  return;
   /* USER CODE END OTG_FS_IRQn 0 */
   HAL_PCD_IRQHandler(&hpcd_USB_OTG_FS);
   /* USER CODE BEGIN OTG_FS_IRQn 1 */
